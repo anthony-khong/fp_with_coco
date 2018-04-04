60de3bf79b66a812747795e78f1d3bd2c42f7b3d
Anthony Khong <akhong@Anthonys-MacBook-Pro.local>
2018-04-04 21:34:45 +0700

    Added pipelines

--- better_sieve.py
+++ better_sieve.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# __coconut_hash__ = 0x23d24269
+# __coconut_hash__ = 0xe685b395
 
 # Compiled with Coconut version 1.3.1 [Dead Parrot]
 
@@ -17,21 +17,10 @@ _coconut_sys.path.remove(_coconut_file_path)
 # Compiled Coconut: -----------------------------------------------------------
 
 @_coconut_tco
-def sieve(*_coconut_match_to_args, **_coconut_match_to_kwargs):
-    _coconut_match_check = False
-    if (_coconut.len(_coconut_match_to_args) == 1) and (_coconut.isinstance(_coconut_match_to_args[0], _coconut.abc.Iterable)):
-        xs = _coconut.iter(_coconut_match_to_args[0])
-        _coconut_match_temp_0 = _coconut.tuple(_coconut_igetitem(xs, _coconut.slice(None, 1)))
-        if (_coconut.len(_coconut_match_temp_0) == 1) and (not _coconut_match_to_kwargs):
-            x = _coconut_match_temp_0[0]
-            _coconut_match_check = True
-    if not _coconut_match_check:
-        _coconut_match_err = _coconut_MatchError("pattern-matching failed for " "'def sieve([x] :: xs) ='" " in " + _coconut.repr(_coconut.repr(_coconut_match_to_args)))
-        _coconut_match_err.pattern = 'def sieve([x] :: xs) ='
-        _coconut_match_err.value = _coconut_match_to_args
-        raise _coconut_match_err
-
-    return _coconut_tail_call(_coconut.itertools.chain.from_iterable, (f() for f in (lambda: [x], lambda: sieve((n for n in xs if n % x)))))
+def sieve(numbers):
+    x = next(numbers)
+    return _coconut_tail_call(_coconut.itertools.chain.from_iterable, (f() for f in (lambda: [x], lambda: sieve(filter(lambda n: n % x != 0, numbers)))))
 
 if __name__ == '__main__':
+    from itertools import count
     (print)((list)(_coconut_igetitem((sieve)(count(2)), _coconut.slice(None, 3000))))
---
+++ tests/test_pipeline.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# __coconut_hash__ = 0x3a0aab9d
+
+# Compiled with Coconut version 1.3.1 [Dead Parrot]
+
+# Coconut Header: -------------------------------------------------------------
+
+from __future__ import generator_stop
+import sys as _coconut_sys, os.path as _coconut_os_path
+_coconut_file_path = _coconut_os_path.dirname(_coconut_os_path.abspath(__file__))
+_coconut_sys.path.insert(0, _coconut_file_path)
+from __coconut__ import _coconut, _coconut_NamedTuple, _coconut_MatchError, _coconut_tail_call, _coconut_tco, _coconut_igetitem, _coconut_base_compose, _coconut_forward_compose, _coconut_back_compose, _coconut_forward_star_compose, _coconut_back_star_compose, _coconut_pipe, _coconut_star_pipe, _coconut_back_pipe, _coconut_back_star_pipe, _coconut_bool_and, _coconut_bool_or, _coconut_none_coalesce, _coconut_minus, _coconut_map, _coconut_partial
+from __coconut__ import *
+_coconut_sys.path.remove(_coconut_file_path)
+
+# Compiled Coconut: -----------------------------------------------------------
+
+import pytest
+
+from pipeline import Estimator
+from pipeline import Transformer
+from pipeline import fit
+from pipeline import transform
+from pipeline import fit_transform
+from pipeline import mappend
+from pipeline import mconcat
+from pipeline import MatchError
+
+def test_fit():
+    estimator = Estimator(lambda x: Transformer(lambda y: x + y))
+    transformer = fit(estimator, 1)
+    assert transformer.transform_fn(2) == 3, 'Fit function on Estimator is incorrect.'
+
+    transformer = Transformer(lambda x: x + 1)
+    fitted_transformer = fit(transformer, 2)
+    assert fitted_transformer.transform_fn(3) == 4, 'Fit function on Transformer incorrect.'
+
+def test_transform():
+    with pytest.raises(MatchError):
+        transform(Estimator(None), 1)
+
+    transformer = Transformer(lambda x: x * x)
+    assert transform(transformer, 5) == 5 * 5, 'Transform function on Transformer incorrect.'
+
+def test_mappend():
+    xformer0 = Transformer(lambda x: x + 1)
+    xformer1 = Transformer(lambda x: x * x)
+    assert mappend(xformer0, xformer1).transform_fn(5) == 36, ('Mappend function on (Transformer, Transformer) incorrect.')
+    assert mappend(xformer1, xformer0).transform_fn(5) == 26, ('Mappend function on (Transformer, Transformer) ignores order.')
+
+    estimator0 = Estimator(lambda _: Transformer(lambda x: x + 1))
+    assert fit_transform(mappend(estimator0, xformer0), 1) == 3, ('Mappend function on (Estimator, Transformer) incorrect.')
+    assert fit_transform(mappend(xformer1, estimator0), 2) == 5, ('Mappend function on (Transformer, Estimator) incorrect.')
+
+    estimator1 = Estimator(lambda _: Transformer(lambda x: x * x * x))
+    assert fit_transform(mappend(estimator0, estimator1), 1) == 8, ('Mappend function on (Estimator, Estimator) incorrect.')
+
+def test_mconcat():
+    stages = [Transformer(lambda x: x + 1), Estimator(lambda x: Transformer(lambda y: x + y)), Transformer(lambda x: x * x), Estimator(lambda x: Transformer(lambda y: x * y))]
+    assert transform(fit(mconcat(stages), 2), 3) == 72, ('Mconcat does not order the stages correctly.')
